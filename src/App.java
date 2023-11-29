9041fd7a784e660d9aefd1665234ab6c81299dce
Christopher <chrisgeop@gmail.com>
2023-11-29 14:34:49 +0200

    changed array name

--- src/App.java
+++ src/App.java
@@ -1,25 +1,25 @@
 public class App {
     public static void main(String[] args)  {
-        int pinakas[]= {15,2,14,6,142,555,777,420};
+        int array[]= {15,2,14,6,142,555,777,420};
         System.out.print("original array: ");
-        for(int i=0; i<pinakas.length; i++) {
-            System.out.print(pinakas[i]+" ");
+        for(int i=0; i<array.length; i++) {
+            System.out.print(array[i]+" ");
         }
-        int n=pinakas.length;
+        int n=array.length;
         for (int i=0; i<n-1; i++) {
             int minimumIndex=i;
             for(int j=i+1; j<n; j++) {
-                if(pinakas[j]<pinakas[minimumIndex]) {
+                if(array[j]<array[minimumIndex]) {
                     minimumIndex=j;
                 }
             }
-            int temp=pinakas[minimumIndex];
-            pinakas[minimumIndex]=pinakas[i];
-            pinakas[i]=temp;
+            int temp=array[minimumIndex];
+            array[minimumIndex]=array[i];
+            array[i]=temp;
         }
         System.out.print("\nordered array: ");
-        for(int i=0; i<pinakas.length; i++) {
-            System.out.print(pinakas[i]+" ");
+        for(int i=0; i<array.length; i++) {
+            System.out.print(array[i]+" ");
         }
     }
     }
